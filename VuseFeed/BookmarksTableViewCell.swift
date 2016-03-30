e0e79f20389c834f4341e9ec68ccf00d8b971ed5
BLbenjaminliu <bl88b@mail.missouri.edu>
2016-03-30 17:16:29 -0500

    bookmarks button
    added popup, configured cell

---
+++ VuseFeed/BookmarksTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookmarksTableViewCell.swift
+//  VuseFeed
+//
+//  Created by MU IT Program on 3/30/16.
+//  Copyright © 2016 Joshua O'Steen. All rights reserved.
+//
+
+import UIKit
+
+class BookmarksTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
