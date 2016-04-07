842402e24ac0f51f57761ec736386e83891c0b21
BLbenjaminliu <bl88b@mail.missouri.edu>
2016-04-07 13:48:20 -0500

    settings added
    finally made settings button navigate to settings page, started
    settings page layout

---
+++ VuseFeed/SettingsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsTableViewCell.swift
+//  VuseFeed
+//
+//  Created by MU IT Program on 4/7/16.
+//  Copyright © 2016 Joshua O'Steen. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
