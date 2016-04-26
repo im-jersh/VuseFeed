2025d165ad23f6f986a2bc9a17b9541d32cbd2f1
oriyentel <joshuaosteen@me.com>
2016-04-26 18:37:54 -0500

    built settings view controller in storyboard and added corresponding class file

---
+++ VuseFeed/SettingsViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SettingsViewController.swift
+//  VuseFeed
+//
+//  Created by Joshua O'Steen on 4/26/16.
+//  Copyright © 2016 Joshua O'Steen. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 4
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section == 1 ? 2 : 1
+    }
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
